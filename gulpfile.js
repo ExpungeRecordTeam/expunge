e0f0d60ff60bb1dd5adab4f4b480a96e754a1290
anthroprose <acorley@anthroprose.com>
2016-06-12 12:50:44 -0500

    removing image optimization (travis errors)

--- gulpfile.js
+++ gulpfile.js
@@ -43,10 +43,10 @@ var AUTOPREFIXER_BROWSERS = [
 // Optimize images
 gulp.task('images', function () {
   return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
+//    .pipe($.cache($.imagemin({
+//      progressive: true,
+//      interlaced: true
+//    })))
     .pipe(gulp.dest('dist/images'))
     .pipe($.size({title: 'images'}));
 });
